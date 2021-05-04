78a7f7976064f2c94865796116e9cb9680d4f295
SAPALASK <SAPALASK@LIN51011983.corp.capgemini.com>
2021-05-04 12:51:33 +0530

    first push

---
+++ PaperPublishingApplicationSpringBoot/src/main/java/com/cg/ppa/PaperPublishingApplicationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cg.ppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperPublishingApplicationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperPublishingApplicationSpringBootApplication.class, args);
+	}
+
+}
